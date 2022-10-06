be58fb6a5d1c17193e9b64da1d8d5773d4812d3a
Victor Nunez <VN135766@gmail.com>
2022-10-06 14:16:13 -0400

    added tech images and fixed visual glitch

--- portfolio-v5/src/components/AboutMe/AboutMe.js
+++ portfolio-v5/src/components/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import classes from './AboutMe.module.css'
 import profilePic from '../../assets/images/profile-picture/profile-cut-out.png'
+import reactMarioBlock from '../../assets/images/technologies/react-mario-block.png'
 
 import Card from '../UI/Card'
 
@@ -7,18 +8,29 @@ const AboutMe = () => {
     return (
         <section className={classes.section}>
             <Card className={classes.profile}>
-                <img src={profilePic} alt="a professional picture of Victor Nunez"/>
+                <img src={profilePic} alt="a professional picture of Victor Nunez" />
             </Card>
-            <div className={classes.aboutMeText} >
-                <h2>Hello!</h2>
-                <br />
-                <h3>My name is <span>Victor Nunez</span>.  I am a <span>Full Stack Web Developer</span> and I am currently located in <span>Livonia, Michigan</span>.</h3>
-                <br />
-                <h3>I just recently earned a certificate from the <span>University of Michigan Coding Bootcamp course</span>.</h3>
-                <br />
-                <h3>I have a passion for building challenging applications and learning new technologies.</h3>
-                <br />
-                <h3>These are a few technologies I use...</h3>
+            <div className={classes.aboutMeContext}>
+                <div className={classes.aboutMeText} >
+                    <h2>Hello!</h2>
+                    <br />
+                    <h3>My name is <span>Victor Nunez</span>.  I am a <span>Full Stack Web Developer</span> and I am currently located in <span>Livonia, Michigan</span>.</h3>
+                    <br />
+                    <h3>I just recently earned a certificate form the <span>University of Michigan Coding Bootcamp course</span>.</h3>
+                    <br />
+                    <h3>I have a passion for building challenging applications and learning new technologies.</h3>
+                    <br />
+                    <h3>These are a few technologies I use...</h3>
+                </div>
+                <div className={classes.technologies}>
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                    <img src={reactMarioBlock} alt="Mario block with React logo" />
+                </div>
             </div>
 
         </section>
