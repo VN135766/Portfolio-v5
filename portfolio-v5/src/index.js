f14b0e58696e46a6aa1e18b01d993e97c5038d4e
Victor Nunez <VN135766@gmail.com>
2022-10-07 23:09:02 -0400

    context now works and user is able to toggle between pages

--- portfolio-v5/src/index.js
+++ portfolio-v5/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PageContextProvider from './store/page-context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(<PageContextProvider><App /></PageContextProvider>, document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
