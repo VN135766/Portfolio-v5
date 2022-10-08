5bd433cf4c9b43c4db65d05090892ce46ca372e3
Victor Nunez <VN135766@gmail.com>
2022-10-07 23:05:57 -0400

    created and styled contact me section

--- portfolio-v5/src/App.js
+++ portfolio-v5/src/App.js
@@ -3,6 +3,8 @@ import AboutMe from './components/AboutMe/AboutMe'
 import Homepage from './components/Homepage/Homepage';
 import PageContext from './store/page-context'
 
+import ContactMe from './components/ContactMe/ContactMe'
+
 function App() {
   const pageCtx = useContext(PageContext)
   console.log(pageCtx)
@@ -11,7 +13,7 @@ function App() {
 {pageCtx.selectedPage === 'Home Page' && <Homepage />}
       {pageCtx.selectedPage === 'About Me' && <AboutMe />}
       {pageCtx.selectedPage === 'Projects' && <AboutMe />}
-      {pageCtx.selectedPage === 'Contact Me' && <AboutMe />}
+      {pageCtx.selectedPage === 'Contact Me' && <ContactMe />}
       </PageContext.Provider>
   );
 }
